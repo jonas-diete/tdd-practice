ee3768256828de2e6ce49120e4eb3e8a49d9eac6
Jonas Diete <jonas.diete@gmail.com>
2022-10-23 21:00:46 +0100

    all tests pass

--- report.test.js
+++ report.test.js
@@ -10,4 +10,13 @@ describe('Report', () => {
   it('returns different scores of one each', () => {
     expect(printReport('Green, Amber, Red')).toBe('Green: 1, Amber: 1, Red: 1');
   });
+  it('returns different scores of one each in any order', () => {
+    expect(printReport('Amber, Green, Red')).toBe('Green: 1, Amber: 1, Red: 1');
+  });
+  it('returns different quantities of each score', () => {
+    expect(printReport('Amber, Green, Red, Green, Red, Green, Green')).toBe('Green: 4, Amber: 1, Red: 2');
+  });
+  it('doesnt return other values', () => {
+    expect(printReport('Amber, Blue, Red, Green, Brown, Green, Green')).toBe('Green: 3, Amber: 1, Red: 1');
+  });
 });
